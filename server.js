880370c5f889e5e595adc4357b52de19b8e35574
Scooterdamus <swilson.webdev@gmail.com>
2022-06-02 21:12:54 -0400

    added process PORT

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/api/:name', (req, res) => {
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is now running on port ${PORT}! Betta GO Catch it!`);
 })
 
